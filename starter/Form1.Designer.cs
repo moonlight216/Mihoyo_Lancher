5de3f223003566ea35ad70f69e2e753e0de512ea
moonlight <1449350237@qq.com>
2023-11-06 21:36:52 +0800

    这里是总结~
    随便写的一个代码，希望能解决第一次启动寻找启动程序慢的问题。

---
+++ starter/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace starter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            more_ys = new Button();
+            ys = new Button();
+            pictureBox1 = new PictureBox();
+            tabPage2 = new TabPage();
+            more_xqtd = new Button();
+            xqtd = new Button();
+            pictureBox2 = new PictureBox();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            toolStripMenuItem1 = new ToolStripMenuItem();
+            toolStripMenuItem3 = new ToolStripMenuItem();
+            toolStripMenuItem2 = new ToolStripMenuItem();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            contextMenuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Dock = DockStyle.Fill;
+            tabControl1.Location = new Point(0, 0);
+            tabControl1.Name = "tabControl1";
+            tabControl1.RightToLeft = RightToLeft.No;
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(1583, 925);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(more_ys);
+            tabPage1.Controls.Add(ys);
+            tabPage1.Controls.Add(pictureBox1);
+            tabPage1.Location = new Point(4, 33);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(1575, 888);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "原神";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // more_ys
+            // 
+            more_ys.Location = new Point(1442, 752);
+            more_ys.Name = "more_ys";
+            more_ys.Size = new Size(44, 70);
+            more_ys.TabIndex = 2;
+            more_ys.Text = "三";
+            more_ys.UseVisualStyleBackColor = true;
+            more_ys.Click += more_ys_Click;
+            // 
+            // ys
+            // 
+            ys.Enabled = false;
+            ys.Font = new Font("微软雅黑", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            ys.Location = new Point(1268, 752);
+            ys.Name = "ys";
+            ys.Size = new Size(175, 70);
+            ys.TabIndex = 1;
+            ys.Text = "开始游戏";
+            ys.UseVisualStyleBackColor = true;
+            ys.Click += ys_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Dock = DockStyle.Fill;
+            pictureBox1.ErrorImage = null;
+            pictureBox1.Image = Properties.Resources.ys;
+            pictureBox1.Location = new Point(3, 3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(1569, 882);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Controls.Add(more_xqtd);
+            tabPage2.Controls.Add(xqtd);
+            tabPage2.Controls.Add(pictureBox2);
+            tabPage2.Location = new Point(4, 33);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(1575, 888);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "崩坏·星穹铁道";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // more_xqtd
+            // 
+            more_xqtd.Location = new Point(1442, 752);
+            more_xqtd.Name = "more_xqtd";
+            more_xqtd.Size = new Size(44, 70);
+            more_xqtd.TabIndex = 3;
+            more_xqtd.Text = "三";
+            more_xqtd.UseVisualStyleBackColor = true;
+            more_xqtd.Click += more_xqtd_Click;
+            // 
+            // xqtd
+            // 
+            xqtd.Enabled = false;
+            xqtd.Font = new Font("微软雅黑", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            xqtd.Location = new Point(1268, 752);
+            xqtd.Name = "xqtd";
+            xqtd.Size = new Size(175, 70);
+            xqtd.TabIndex = 2;
+            xqtd.Text = "开始游戏";
+            xqtd.UseVisualStyleBackColor = true;
+            xqtd.Click += xqtd_Click;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Dock = DockStyle.Fill;
+            pictureBox2.ErrorImage = null;
+            pictureBox2.Image = Properties.Resources.td;
+            pictureBox2.Location = new Point(3, 3);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(1569, 882);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 0;
+            pictureBox2.TabStop = false;
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.ImageScalingSize = new Size(24, 24);
+            contextMenuStrip1.Items.AddRange(new ToolStripItem[] { toolStripMenuItem1, toolStripMenuItem3, toolStripMenuItem2 });
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(243, 127);
+            // 
+            // toolStripMenuItem1
+            // 
+            toolStripMenuItem1.Name = "toolStripMenuItem1";
+            toolStripMenuItem1.Size = new Size(242, 30);
+            toolStripMenuItem1.Text = "选择游戏启动器目录";
+            toolStripMenuItem1.Click += toolStripMenuItem1_Click;
+            // 
+            // toolStripMenuItem3
+            // 
+            toolStripMenuItem3.Name = "toolStripMenuItem3";
+            toolStripMenuItem3.Size = new Size(242, 30);
+            toolStripMenuItem3.Text = "重置配置文件";
+            toolStripMenuItem3.Click += toolStripMenuItem3_Click;
+            // 
+            // toolStripMenuItem2
+            // 
+            toolStripMenuItem2.Name = "toolStripMenuItem2";
+            toolStripMenuItem2.Size = new Size(242, 30);
+            toolStripMenuItem2.Text = "软件信息";
+            toolStripMenuItem2.Click += toolStripMenuItem2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(11F, 24F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1583, 925);
+            Controls.Add(tabControl1);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "启动器";
+            Load += Form1_Load;
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            contextMenuStrip1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private Button ys;
+        private PictureBox pictureBox1;
+        private TabPage tabPage2;
+        private Button more_ys;
+        private Button more_xqtd;
+        private Button xqtd;
+        private PictureBox pictureBox2;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem toolStripMenuItem1;
+        private ToolStripMenuItem toolStripMenuItem2;
+        private ToolStripMenuItem toolStripMenuItem3;
+    }
+}
---
+++ starter/Form1.cs
@@ -0,0 +1,303 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace starter
+{
+    public partial class Form1 : Form
+    {
+        String lanchPath_ys = null;
+        String lanchPath_td = null;
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        public class Config
+        {
+            [System.Runtime.InteropServices.DllImport("kernel32")]
+            private static extern long WritePrivateProfileString(string section, string key, string val, string filePath);
+
+            [System.Runtime.InteropServices.DllImport("kernel32")]
+            private static extern int GetPrivateProfileString(string section, string key, string def, System.Text.StringBuilder retVal, int size, string filePath);
+
+            public void configWrite(string section, string key, string value, string path)
+            {
+                // section=配置节点名称，key=键名，value=返回键值，path=路径
+                WritePrivateProfileString(section, key, value, path);
+            }
+
+            //读取INI的方法
+            public string configRead(string section, string key, string path)
+            {
+                StringBuilder temp = new StringBuilder(255);
+
+                GetPrivateProfileString(section, key, "", temp, 255, path);
+                return temp.ToString();
+            }
+
+            public void configDelete(string FilePath)
+            {
+                File.Delete(FilePath);
+            }
+        }
+
+        public class LancherSelector
+        {
+            int delnum;
+            int delnum2;
+            string lancherName;
+            PictureBox pictureBox;
+            Button button;
+            string bgName;
+            public string path = null;
+
+            public LancherSelector(int delnum, int delnum2, string lancherName, PictureBox pictureBox, Button button, string bgName)
+            {
+                this.delnum = delnum;
+                this.lancherName = lancherName;
+                this.pictureBox = pictureBox;
+                this.button = button;
+                this.bgName = bgName;
+                this.delnum2 = delnum2;
+            }
+            public void SearchLancher()//"YuanShen.exe"
+            {
+                button.Text = "加载中...";
+                //TO DO 自动获取启动程序路径
+                DriveInfo[] allDrives = DriveInfo.GetDrives();
+                foreach (DriveInfo d in allDrives)
+                {
+                    string d_name = d.Name;
+                    string result = SearchForFile(d_name, lancherName);
+                    if (result != null)
+                    {
+                        path = result[..^delnum];
+                        Config config = new Config();
+                        config.configWrite("Path_Record", lancherName, path, Environment.CurrentDirectory + "\\config.ini");
+                        break;
+                    }
+                }
+                Loadbg(path, delnum2, pictureBox, button, bgName);
+                GC.Collect();
+            }
+        }
+
+        LancherSelector ls_ys;
+        LancherSelector ls_td;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string config_path = Environment.CurrentDirectory + "\\config.ini";
+            Config config = new Config();
+            if (File.Exists(config_path))
+            {
+                lanchPath_ys = config.configRead("Path_Record", "YuanShen.exe", config_path);
+                lanchPath_td = config.configRead("Path_Record", "StarRail.exe", config_path);
+                if (lanchPath_ys != null && lanchPath_ys != "")
+                {
+                    Loadbg(lanchPath_ys, 20, pictureBox1, ys, "ys.png");
+                }
+                else
+                {
+                    ls_ys = new LancherSelector(13, 20, "YuanShen.exe", pictureBox1, ys, "ys.png");
+                    Thread thread_ys = new Thread(new ThreadStart(ls_ys.SearchLancher));
+                    thread_ys.Start();
+                }
+
+                if (lanchPath_td != null && lanchPath_td != "")
+                {
+                    Loadbg(lanchPath_td, 5, pictureBox2, xqtd, "td.png");
+                }
+                else
+                {
+                    ls_td = new LancherSelector(13, 5, "StarRail.exe", pictureBox2, xqtd, "td.png");
+                    Thread thread_td = new Thread(new ThreadStart(ls_td.SearchLancher));
+                    thread_td.Start();
+                }
+            }
+            else
+            {
+                ls_ys = new LancherSelector(13, 20, "YuanShen.exe", pictureBox1, ys, "ys.png");
+                Thread thread_ys = new Thread(new ThreadStart(ls_ys.SearchLancher));
+                thread_ys.Start();
+
+                ls_td = new LancherSelector(13, 5, "StarRail.exe", pictureBox2, xqtd, "td.png");
+                Thread thread_td = new Thread(new ThreadStart(ls_td.SearchLancher));
+                thread_td.Start();
+            }
+
+
+
+            //SearchLancher(out lanchPath_ys, 13, "YuanShen.exe");
+            //SearchLancher(out lanchPath_td, 13, "StarRail.exe");
+
+            //lanchPath_ys = "E:\\Genshin Impact\\Genshin Impact Game";
+            //Loadbg(lanchPath_ys, 20, pictureBox1, ys, "ys.png");
+            //lanchPath_td = "E:\\Star Rail\\Game";
+            //Loadbg(lanchPath_td, 5, pictureBox2, xqtd, "td.png");
+        }
+
+        private static void Loadbg(string lanchpath, int delnum, PictureBox pictureBox, Button button, string bgName)
+        {
+            if (lanchpath != null)
+            {
+                try
+                {
+                    string bgPath = lanchpath[..^delnum];
+                    string[] picPath = Directory.GetFiles(bgPath + "\\bg", "*.png", searchOption: SearchOption.AllDirectories);
+                    if (picPath.Length > 1)
+                    {
+                        Random r = new Random();
+                        pictureBox.Image = Image.FromFile(picPath[r.Next(0, picPath.Length)]);
+
+                    }
+                    else
+                    {
+                        pictureBox.Image = Image.FromFile(picPath[0]);
+                    }
+                    button.Font = new Font("微软雅黑", 14);
+                    button.Text = "开始游戏";
+                    button.Enabled = true;
+                }
+                catch (Exception)
+                {
+                    Config config = new Config();
+                    config.configDelete(Environment.CurrentDirectory + "\\config.ini");
+                    button.Enabled = false;
+                    button.Font = new Font("微软雅黑", 10);
+                    button.Text = "请手动选择-->";
+                    pictureBox.Image = Image.FromFile(Environment.CurrentDirectory + "\\bg\\" + bgName);
+                }
+            }
+            else
+            {
+                button.Enabled = false;
+                button.Font = new Font("微软雅黑", 10);
+                button.Text = "请手动选择-->";
+                pictureBox.Image = Image.FromFile(Environment.CurrentDirectory + "\\bg\\" + bgName);
+            }
+        }
+
+        private static string SearchForFile(string d_name, string filename)
+        {
+            String path = null;
+            try
+            {
+                var files = Directory.GetFiles(d_name, filename, SearchOption.TopDirectoryOnly);
+                if (files.Length > 0)
+                {
+                    path = files.First();
+                }
+                else
+                {
+                    var subDirectories = Directory.GetDirectories(d_name);
+                    foreach (var subDirectory in subDirectories)
+                    {
+                        path = SearchForFile(subDirectory, filename);
+                        if (path != null)
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                Console.WriteLine("UnauthorizedAccessException");
+            }
+            catch (DirectoryNotFoundException)
+            {
+                MessageBox.Show("DirectoryNotFoundException");
+            }
+            return path;
+        }
+
+        private void xqtd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (lanchPath_td == null)
+                {
+                    lanchPath_td = ls_td.path;
+                }
+                Process.Start(lanchPath_td + "\\StarRail.exe");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("请以管理员身份启动程序", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+        }
+
+        private void ys_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (lanchPath_ys == null)
+                {
+                    lanchPath_ys = ls_ys.path;
+                }
+                Process.Start(lanchPath_ys + "\\YuanShen.exe");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("请以管理员身份启动程序", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+        }
+
+        string gameName;
+        private void more_ys_Click(object sender, EventArgs e)
+        {
+            gameName = "YuanShen.exe";
+            contextMenuStrip1.Show(more_ys, new Point(more_ys.Width - 200, more_ys.Height - 170));
+        }
+
+        private void more_xqtd_Click(object sender, EventArgs e)
+        {
+            gameName = "StarRail.exe";
+            contextMenuStrip1.Show(more_ys, new Point(more_ys.Width - 200, more_ys.Height - 170));
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = gameName + "|" + gameName;
+            ofd.Multiselect = false;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                if (gameName.Equals("YuanShen.exe"))
+                {
+                    lanchPath_ys = ofd.FileName[..^13];
+                    Config config = new Config();
+                    config.configWrite("Path_Record", "YuanShen.exe", lanchPath_ys, Environment.CurrentDirectory + "\\config.ini");
+                    Loadbg(lanchPath_ys, 20, pictureBox1, ys, "ys.png");
+                }
+                else if (gameName.Equals("StarRail.exe"))
+                {
+                    lanchPath_td = ofd.FileName[..^13];
+                    Config config = new Config();
+                    config.configWrite("Path_Record", "StarRail.exe", lanchPath_td, Environment.CurrentDirectory + "\\config.ini");
+                    Loadbg(lanchPath_td, 5, pictureBox2, xqtd, "td.png");
+                }
+            }
+        }
+
+        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("确认重置配置文件吗？", "提示", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+            {
+                Config config = new Config();
+                config.configDelete(Environment.CurrentDirectory + "\\config.ini");
+                Application.Restart();
+            };
+
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("qaq");
+        }
+    }
+}
